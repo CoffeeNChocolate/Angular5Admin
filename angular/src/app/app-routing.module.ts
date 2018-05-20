b2b517ea3567a74ffdfee3a1958b04260f83c1a3
Digvijay Singh <digvijay_si@HCL.COM>
2018-05-20 22:00:30 +0530

    Initial Commit

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LayoutComponent } from './/layouts/layout.component';
+
+import { HomeComponent } from './pages/home/home.component';
+
+import { ColorsComponent } from './pages/ui/colors/colors.component';
+import { TypographyComponent } from './pages/ui/typography/typography.component';
+import { PanelsComponent } from './pages/ui/panels/panels.component';
+import { TabsComponent } from './pages/ui/tabs/tabs.component';
+import { AlertsComponent } from './pages/ui/alerts/alerts.component';
+import { CardsComponent } from './pages/ui/cards/cards.component';
+import { BadgesProgressComponent } from './pages/ui/badges-progress/badges-progress.component';
+import { ListComponent } from './pages/ui/list/list.component';
+import { IconsComponent } from './pages/ui/icons/icons.component';
+import { ButtonsComponent } from './pages/ui/buttons/buttons.component';
+import { TablesComponent } from './pages/tables/tables.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: 'index', pathMatch: 'full'},
+    {
+        "path": "",
+        "component": LayoutComponent,
+        "children": [
+            {
+                path: "index",
+                component: HomeComponent
+            },
+            {
+                path: "table",
+                component: TablesComponent
+            },
+           
+           
+        ]
+    },
+    
+    {
+        "path": "**",
+        "redirectTo": "error_404",
+        "pathMatch": "full"
+    },
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ColorsComponent,
+    TypographyComponent,
+    PanelsComponent,
+    TabsComponent,
+    AlertsComponent,
+    CardsComponent,
+    BadgesProgressComponent,
+    ListComponent,
+    IconsComponent,
+    ButtonsComponent,
+   
+  ],
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ 
+    RouterModule,
+  ]
+})
+
+export class AppRoutingModule { }
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './/app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LayoutModule } from './/layouts/layout.module';
+import { ScriptLoaderService } from './_services/script-loader.service';
+import { TablesComponent } from './pages/tables/tables.component';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    FormsModule,
+    TableModule,
+    HttpClientModule,
+    InputTextModule,
+    DialogModule,
+    ButtonModule
+  ],
+  providers: [ScriptLoaderService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
